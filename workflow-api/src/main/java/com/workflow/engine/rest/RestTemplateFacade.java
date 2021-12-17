5f523b9b5922ceccebe4e8c9f53866b14d735d37
DavidJMartin <davidjmartin@hotmail.com>
2021-12-17 22:28:00 +0000

    added restTemplateFacade to workflow-api

---
+++ workflow-api/src/main/java/com/workflow/engine/rest/RestTemplateFacade.java
@@ -0,0 +1,15 @@
+package com.workflow.engine.rest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author davidjmartin
+ */
+public class RestTemplateFacade {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+}
